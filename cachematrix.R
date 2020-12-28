6b669f8cb5852edcf8c27938e8d57a133e9b8c10
cmtuason1 <cmtuason1@up.edu.ph>
2020-12-28 15:04:05 +0800

    Coursera Assignment 2

---
+++ cachematrix.R
@@ -0,0 +1,37 @@
+## The main objective of this function is to cache the inverse of matrix since
+## matrix inversion has costly computation.
+
+## creates a special "matrix" object that can cache its inverse
+
+makeCacheMatrix <- function(x = matrix()){
+  inv <- NULL
+  set <- function(y){
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function(){
+    x
+  }
+  setInverse <- function(inverse){
+    inv <<- inverse
+  }
+  getInverse <- function(){
+    inv
+  }
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
+}
+
+
+## computes the inverse of the special "matrix" returned by makeCacheMatrix
+
+cacheSolve <- function(x, ...){
+  inv <- x$getInverse()
+  if(!is.null(inv)){
+    message("getting cached data")
+    return(inv)
+  }
+  mat <- x$get()
+  inv <- solve(mat, ...)
+  x$setInverse(inv)
+  inv
+}
